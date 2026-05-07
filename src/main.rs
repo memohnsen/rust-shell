f7f959dab2444dc32907b3c43229d9ede9a6e9da
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-07 19:57:43 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ use find_in_path::prelude::*;
 use std::io::{self, Write};
 use std::path::PathBuf;
 
+fn handle_echo_command(stripped_command: &str) {
+    // echo repeats the arg the user input
+    println!("{stripped_command}");
+}
+
+fn handle_type_command(stripped_command: &str, valid_commands: [&str; 3]) {
+    let command_in_path: Option<PathBuf> = stripped_command.to_string().find_in_path();
+
+    // type says what the arg is, built in or not
+    if valid_commands.contains(&stripped_command) {
+        println!("{stripped_command} is a shell builtin");
+    } else if let Some(path) = command_in_path {
+        // go through every dir in path, check if file with command name exists
+        // if exists and has execute perms, print ,else continue searching
+        println!("{stripped_command} is { }", path.to_str().expect("Invalid"));
+    } else {
+        println!("{stripped_command}: not found");
+    }
+}
+
 fn main() {
     const VALID_COMMANDS: [&str; 3] = ["type", "exit", "echo"];
 
@@ -20,21 +40,9 @@ fn main() {
         if command == "exit" {
             break;
         } else if let Some(stripped) = command.strip_prefix("echo ") {
-            // echo repeats the arg the user input
-            println!("{stripped}");
+            handle_echo_command(stripped);
         } else if let Some(stripped_command) = command.strip_prefix("type ") {
-            let command_in_path: Option<PathBuf> = stripped_command.to_string().find_in_path();
-
-            // type says what the arg is, built in or not
-            if VALID_COMMANDS.contains(&stripped_command) {
-                println!("{stripped_command} is a shell builtin");
-            } else if let Some(path) = command_in_path {
-                // go through every dir in path, check if file with command name exists
-                // if exists and has execute perms, print ,else continue searching
-                println!("{stripped_command} is { }", path.to_str().expect("Invalid"));
-            } else {
-                println!("{stripped_command}: not found");
-            }
+            handle_type_command(stripped_command, VALID_COMMANDS);
         } else {
             println!("{command}: command not found");
         };
