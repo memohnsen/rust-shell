11eb7364fc166d1a070f2e1d4cdd8c7bec1f8590
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-07 19:31:19 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use find_in_path::prelude::*;
 use std::io::{self, Write};
+use std::path::PathBuf;
 
 fn main() {
     const VALID_COMMANDS: [&str; 3] = ["type", "exit", "echo"];
@@ -18,10 +20,16 @@ fn main() {
         if command == "exit" {
             break;
         } else if let Some(stripped) = command.strip_prefix("echo ") {
+            // echo repeats the arg the user input
             println!("{stripped}");
         } else if let Some(stripped_command) = command.strip_prefix("type ") {
-            if VALID_COMMANDS.contains(&stripped_command) {
-                println!("{stripped_command} is a shell builtin");
+            // go through every dir in path, check if file with command name exists
+            // if exists and has execute perms, print ,else continue searching
+            let stripped_command = stripped_command.to_string();
+            let command_in_path: Option<PathBuf> = stripped_command.find_in_path();
+
+            if let Some(path) = command_in_path {
+                println!("command is {:?}", path)
             } else {
                 println!("{stripped_command}: not found");
             }
