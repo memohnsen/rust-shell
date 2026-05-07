e93113d7a59e06fa05968ad28aeb96083e121f8b
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-07 18:20:52 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,12 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+
+    io::stdin()
+        .read_line(&mut command)
+        .expect("{command}: command not found");
+
+    println!("{command}: command not found");
 }
