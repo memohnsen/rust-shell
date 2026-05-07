15a3ef8a7b5f854e6f559078688bb59057ed48b7
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-07 19:49:07 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, Write};
 use std::path::PathBuf;
 
 fn main() {
-    // const VALID_COMMANDS: [&str; 3] = ["type", "exit", "echo"];
+    const VALID_COMMANDS: [&str; 3] = ["type", "exit", "echo"];
 
     loop {
         print!("$ ");
@@ -23,12 +23,14 @@ fn main() {
             // echo repeats the arg the user input
             println!("{stripped}");
         } else if let Some(stripped_command) = command.strip_prefix("type ") {
-            // go through every dir in path, check if file with command name exists
-            // if exists and has execute perms, print ,else continue searching
-            let stripped_command = stripped_command.to_string();
-            let command_in_path: Option<PathBuf> = stripped_command.find_in_path();
+            let command_in_path: Option<PathBuf> = stripped_command.to_string().find_in_path();
 
-            if let Some(path) = command_in_path {
+            // type says what the arg is, built in or not
+            if VALID_COMMANDS.contains(&stripped_command) {
+                println!("{stripped_command} is a shell builtin");
+            } else if let Some(path) = command_in_path {
+                // go through every dir in path, check if file with command name exists
+                // if exists and has execute perms, print ,else continue searching
                 println!("{stripped_command} is { }", path.to_str().expect("Invalid"));
             } else {
                 println!("{stripped_command}: not found");
