b452a8a365e96626b76e57d47086de0ce8b86397
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-16 10:16:47 -0400

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -69,13 +69,13 @@ impl Commands {
     pub fn handle_executable_command(command: &[String]) {
         // find file or exe in path
         let command_in_path: Option<PathBuf> = command[0].find_in_path();
-        let arg1 = &command[1];
+        let args = &command[1..];
 
         // go through every dir in path, check if file with command name exists
         // if exists and has execute perms, execute
         if let Some(path) = command_in_path {
             let output = Command::new(path)
-                .arg(arg1)
+                .args(args)
                 .output()
                 .expect("Error running program");
             let output_str = String::from_utf8_lossy(&output.stdout);
