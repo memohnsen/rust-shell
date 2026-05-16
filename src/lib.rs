d74230684cffb2cc898d4d15493149db3ef6069e
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-15 21:17:50 -0400

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use find_in_path::FindInPath;
-use std::{path::PathBuf, process};
+use std::{
+    path::PathBuf,
+    process::{self, Command, ExitStatus},
+};
 
 pub enum Commands {
     Type(String),
@@ -42,6 +45,7 @@ impl Commands {
     }
 
     pub fn handle_type_command(command: &str, valid_commands: [&str; 3]) {
+        // find file or exe in path
         let command_in_path: Option<PathBuf> = command.to_string().find_in_path();
 
         // type says what the arg is, built in or not
@@ -54,6 +58,21 @@ impl Commands {
                 "{command} is {}",
                 path.to_str().expect("Error converitng file path to string")
             );
+        } else {
+            println!("{command}: not found");
+        }
+    }
+
+    pub fn handle_executable_command(command: &str) {
+        // find file or exe in path
+        let command_in_path: Option<PathBuf> = command.to_string().find_in_path();
+
+        // go through every dir in path, check if file with command name exists
+        // if exists and has execute perms, execute
+        if let Some(path) = command_in_path {
+            let output = Command::new(path).arg("").arg("").output().expect("");
+            let output_str = String::from_utf8_lossy(&output.stdout);
+            println!("{output_str}");
         } else {
             println!("{command}: not found");
         }
