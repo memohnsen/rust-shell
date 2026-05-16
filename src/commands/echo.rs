e085b2805574156df8283349f50b58a2d73712c4
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-16 17:20:34 -0400

    codecrafters submit [skip ci]

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -2,7 +2,7 @@ pub fn execute(command: &str) {
     if command.starts_with("'") {
         println!("{}", command.trim_matches('\''));
     } else {
-        let trimmed: String = command.split_whitespace().collect();
+        let trimmed: String = command.split_whitespace().collect::<Vec<&str>>().join(" ");
         println!("{trimmed}");
     }
 }
