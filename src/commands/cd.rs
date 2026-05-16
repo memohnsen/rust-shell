d28e4ce54e0dd2aaaa7ed1a5519b46206a5a88cf
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-16 16:50:55 -0400

    codecrafters submit [skip ci]

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -1,17 +1,18 @@
 use std::{env, path::Path};
 
 pub fn execute(command: &str) {
-    // TODO: handle all path types:
-    // absolute - full path from /
-    // relative - ./ ../
-    // ~ from home dir
-    //
-    // for absolute, if dir go to dir
-    // else cd: dir: No such file or directory
-
     let new_dir = Path::new(command);
-    match env::set_current_dir(new_dir) {
-        Ok(_) => (),
-        Err(_) => println!("cd: {}: No such file or directory", new_dir.display()),
-    };
+    let home_dir = env::home_dir().expect("cd: {new_dir}: No such file or directory");
+
+    if new_dir == "~" {
+        match env::set_current_dir(home_dir) {
+            Ok(_) => (),
+            Err(_) => println!("cd: {}: No such file or directory", new_dir.display()),
+        };
+    } else {
+        match env::set_current_dir(new_dir) {
+            Ok(_) => (),
+            Err(_) => println!("cd: {}: No such file or directory", new_dir.display()),
+        };
+    }
 }
