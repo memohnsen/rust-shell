862f18467a8034029cb5f9b9f43ab37544d880c9
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-17 12:55:19 -0400

    codecrafters submit [skip ci]

--- src/commands/executable.rs
+++ src/commands/executable.rs
@@ -1,5 +1,5 @@
 use find_in_path::FindInPath;
-use std::{fs::File, io::Write, os::unix::process::CommandExt, path::PathBuf, process::Command};
+use std::{os::unix::process::CommandExt, path::PathBuf, process::Command};
 
 pub fn execute(command: &[String]) {
     if command.is_empty() {
@@ -7,16 +7,17 @@ pub fn execute(command: &[String]) {
     }
 
     let command_in_path: Option<PathBuf> = command[0].find_in_path();
+    let args = &command[1..];
 
     if command.contains(&">".to_string()) || command.contains(&"1>".to_string()) {
-        let mut file = File::create(&command[command.len() - 1]).unwrap();
-        let output = command[1..command.len() - 2].join(" ");
-
-        file.write_all(output.as_bytes()).unwrap();
-        file.write_all(b"\n").unwrap();
+        let output = Command::new(&command[0])
+            .arg0(&command[0])
+            .args(args)
+            .output()
+            .expect("Error running program");
+        let output_str = String::from_utf8_lossy(&output.stderr);
+        print!("{output_str}");
     } else if let Some(path) = command_in_path {
-        let args = &command[1..];
-
         let output = Command::new(path)
             .arg0(&command[0])
             .args(args)
