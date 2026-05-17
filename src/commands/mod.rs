7dbfc631a9d90d8ce026b07d73e30c0d9d8c4924
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-17 09:41:37 -0400

    codecrafters submit [skip ci]

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,3 @@
-pub mod cat;
 pub mod cd;
 pub mod echo;
 pub mod executable;
@@ -7,7 +6,7 @@ pub mod r#type;
 
 use std::process;
 
-pub const BUILTIN_COMMANDS: [&str; 6] = ["type", "exit", "echo", "pwd", "cd", "cat"];
+pub const BUILTIN_COMMANDS: [&str; 5] = ["type", "exit", "echo", "pwd", "cd"];
 
 pub enum Command {
     Type(String),
@@ -16,7 +15,6 @@ pub enum Command {
     Executable(Vec<String>),
     Pwd,
     Cd(String),
-    Cat(Vec<String>),
 }
 
 impl Command {
@@ -29,7 +27,6 @@ impl Command {
             Command::Type(command) => r#type::execute(command),
             Command::Echo(command) => echo::execute(command),
             Command::Executable(command) => executable::execute(command),
-            Command::Cat(command) => cat::execute(command),
             Command::Pwd => pwd::execute(),
             Command::Cd(command) => cd::execute(command),
             Command::Exit => process::exit(0),
--- src/parser.rs
+++ src/parser.rs
@@ -14,18 +14,12 @@ pub fn parse(input: &str) -> Command {
     } else if input.starts_with("cd ") {
         let stripped = input.strip_prefix("cd ").unwrap();
         Command::Cd(String::from(stripped))
-    } else if input.starts_with("cat ") {
-        let args: Vec<String> = input
-            .split("'")
-            .filter(|word| word != &"")
-            .map(|s| s.to_string())
-            .collect();
-        Command::Executable(args)
     } else {
         let args: Vec<String> = input
             .split_ascii_whitespace()
             .map(|s| s.to_string())
             .collect();
+
         Command::Executable(args)
     }
 }
