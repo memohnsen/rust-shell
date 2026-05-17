e1da53ca72e113c5c9e1907ef06d990badc29e15
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-17 12:52:03 -0400

    codecrafters submit [skip ci]

--- src/commands/executable.rs
+++ src/commands/executable.rs
@@ -1,5 +1,5 @@
 use find_in_path::FindInPath;
-use std::{os::unix::process::CommandExt, path::PathBuf, process::Command};
+use std::{fs::File, io::Write, os::unix::process::CommandExt, path::PathBuf, process::Command};
 
 pub fn execute(command: &[String]) {
     if command.is_empty() {
@@ -7,9 +7,16 @@ pub fn execute(command: &[String]) {
     }
 
     let command_in_path: Option<PathBuf> = command[0].find_in_path();
-    let args = &command[1..];
 
-    if let Some(path) = command_in_path {
+    if command.contains(&">".to_string()) || command.contains(&"1>".to_string()) {
+        let mut file = File::create(&command[command.len() - 1]).unwrap();
+        let output = command[1..command.len() - 2].join(" ");
+
+        file.write_all(output.as_bytes()).unwrap();
+        file.write_all(b"\n").unwrap();
+    } else if let Some(path) = command_in_path {
+        let args = &command[1..];
+
         let output = Command::new(path)
             .arg0(&command[0])
             .args(args)
